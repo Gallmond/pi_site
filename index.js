244f4ce15b58bc76eba74ae77398ba9473803634
gavin <gallmond@gmail.com>
2018-04-24 19:03:38 +0100

    syntax

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.use(express.static(__dirname + '/public'));
 // grab body data and insert into request.rawBody
 app.use((req, res, next)=>{
    var data = "";
-   req.on('data', (chunk){ data += chunk})
-   req.on('end', (){
+   req.on('data', (chunk)=>{ data += chunk})
+   req.on('end', ()=>{
 	   req.rawBody = data;
 	   next();
    });
